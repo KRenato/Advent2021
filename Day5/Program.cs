10e0662ab6fe00669fa18436e55ebf1f0a4569f7
Kevin Renato <kev.renato@gmail.com>
2022-11-17 16:57:34 -0500

    Added day 5 template and changed all projects to .net 7.

---
+++ Day5/Program.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+
+var input = await InputReader.GetInputAsync();
+
+Console.WriteLine("Part 1" + Environment.NewLine);
+
+Part1(input);
+
+Console.WriteLine(Environment.NewLine + "Part 2" + Environment.NewLine);
+
+Part2(input);
+
+static void Part1(string[] input)
+{
+}
+
+static void Part2(string[] input)
+{
+}
